ab724f02be56ee680080c4a2d84c1a3bf77d4783
Ryan <ryan@localhost.localdomain>
2021-11-29 15:55:20 -0700

    Initial commit

--- main.py
+++ main.py
@@ -5,39 +5,61 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Ariel"
 
-with open("data/french_words.csv") as csvfile:
-    records = csv.DictReader(csvfile)
-    translation_dict = [row for row in records]
+class Card():
+    def __init__(self):
 
-def next_card():
-    choice = random.randint(0, len(translation_dict) - 1)
-    english_translation = translation_dict[choice]["English"]
-    french_translation = translation_dict[choice]["French"]
+        self.deck = []
+        self.current_card = {}
+        self.load_cards()
+        self.timer = root.after(3000, self.flip_card)
 
-    card_canvas.itemconfig(french_word, text=french_translation)
+        # UI setup
+        # Canvas
+        self.canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+        self.card_front = PhotoImage(file="images/card_front.png")
+        self.card_back = PhotoImage(file="images/card_back.png")
+
+        self.current_side = self.canvas.create_image(405,263, image=self.card_front)
+        self.language = self.canvas.create_text(400,150, text="", font=(FONT_NAME, 40, "italic"))
+        self.word = self.canvas.create_text(400,263, text="", font=(FONT_NAME, 60, "bold"))
+        self.canvas.grid(column=1, row=0, columnspan=2, rowspan=2)
+
+        # Buttons
+        self.right_img = PhotoImage(file="images/right.png")
+        self.wrong_img = PhotoImage(file="images/wrong.png")
+        self.wrong_button = Button(image=self.wrong_img, highlightthickness=0, bd=0, bg=BACKGROUND_COLOR, command=self.next_card)
+        self.right_button = Button(image=self.right_img, highlightthickness=0, bd=0, bg=BACKGROUND_COLOR, command=self.next_card)
+        self.right_button.grid(column=1,row=2)
+        self.wrong_button.grid(column=2,row=2)
+        
+    def load_cards(self):
+        with open("data/french_words.csv") as csvfile:
+            records = csv.DictReader(csvfile)
+            self.deck = [row for row in records]
+    def next_card(self):
+        # Might want to change how this is done entirely
+        # could add a flag to see if the card is on front or back
+        root.after_cancel(self.timer)
+        choice = random.randint(0, len(self.deck) - 1)
+        self.current_card = self.deck[choice]
     
+        self.canvas.itemconfig(self.current_side, image=self.card_front)
+        self.canvas.itemconfig(self.language, text="French", fill="black")
+        self.canvas.itemconfig(self.word, text=self.current_card["French"], fill="black")
+
+        self.timer = root.after(3000, self.flip_card)
+    def flip_card(self):
+        self.canvas.itemconfig(self.current_side, image=self.card_back)
+        self.canvas.itemconfig(self.language, text="English", fill="white")
+        self.canvas.itemconfig(self.word, text=self.current_card["English"], fill="white")
+
 # Window
 root = Tk()
 root.title("Flashy")
 root.config(bg=BACKGROUND_COLOR, pady=50, padx=50)
 
-# Canvas
-card_back_img = PhotoImage(file="images/card_back.png")
-card_front_img = PhotoImage(file="images/card_front.png")
+card = Card()
 
-card_canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-card_canvas.create_image(405,263, image=card_front_img)
-english_word = card_canvas.create_text(400,150, text="French", font=(FONT_NAME, 40, "italic"))
-french_word = card_canvas.create_text(400,263, text=translation_dict[0]["French"], font=(FONT_NAME, 60, "bold"))
-card_canvas.grid(column=1, row=0, columnspan=2, rowspan=2)
-
-# Buttons
-right_img = PhotoImage(file="images/right.png")
-wrong_img = PhotoImage(file="images/wrong.png")
-right_button = Button(image=right_img, highlightthickness=0, bd=0, bg=BACKGROUND_COLOR, command=next_card)
-wrong_button = Button(image=wrong_img, highlightthickness=0, bd=0, bg=BACKGROUND_COLOR, command=next_card)
-right_button.grid(column=1,row=2)
-wrong_button.grid(column=2,row=2)
-
-# Labels
+# Main loop
+card.next_card()
 root.mainloop()
